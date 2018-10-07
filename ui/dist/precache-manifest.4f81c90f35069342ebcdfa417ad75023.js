4e9d0fb295aa47ae983b1698f831c88122d30777
Alex Wohlbruck <alexwohlbruck@gmail.com>
2018-10-07 06:26:41 -0400

    Build code

---
+++ ui/dist/precache-manifest.4f81c90f35069342ebcdfa417ad75023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5f9570cd65fbde0e9024",
+    "url": "/js/chunk-vendors.3775a126.js"
+  },
+  {
+    "revision": "12bf07412bdefb975fd7",
+    "url": "/js/app.9e282184.js"
+  },
+  {
+    "revision": "fd58e3a1967705d82befc2e1deb551dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f9570cd65fbde0e9024",
+    "url": "/css/chunk-vendors.09bd4d3c.css"
+  },
+  {
+    "revision": "12bf07412bdefb975fd7",
+    "url": "/css/app.9c3c2c97.css"
+  }
+];
--- ui/src/views/Add.vue
+++ ui/src/views/Add.vue
@@ -1,18 +1,26 @@
 <template lang="pug">
-  .add.md-layout
-    .md-layout-item.md-size-50.padding
-      md-field
-        label Input
-        md-input(v-model='input')
-        
-      md-field
-        label Input
-        md-input(v-model='input')
+  div
+    md-dialog-prompt(:md-active.sync='newAccountDialog', v-model='newAccount', md-title='Create new account', md-input-placeholder='Account name', md-confirm-text='Create')
 
-    .md-layout-item.md-size-50
-      md-list
-        NestedItem
+    .add.md-layout
+      .md-layout-item.md-size-50.padding
+        md-field
+          label(for='account') Account
+          md-select#account(v-model='account', name='account', @md-selected='accountSelected')
+            md-option(value='1') 1
+            md-option(value='new') New account
+          
+        md-field
+          label Value
+          md-input(v-model='input')
+          
+
+        p {{account}}
+
+      .md-layout-item.md-size-50
+        md-list
           NestedItem
+            NestedItem
 </template>
 
 <script>
@@ -21,10 +29,19 @@
   export default {
     name: 'add',
     data: () => ({
+      newAccount: null,
+      newAccountDialog: false,
       input: 'test'
     }),
     components: {
       NestedItem
+    },
+    methods: {
+      accountSelected() {
+        alert(this.newAccount)
+        if (this.newAccount == 'new')
+          this.newAccountDialog = true
+      }
     }
   }
 </script>
